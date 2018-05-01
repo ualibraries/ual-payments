00dba150f0b0c2bd952e2cc4eec678d0ea1d39b9
Andy Osborne <cao89@email.arizona.edu>
2018-05-01 08:46:36 -0700

    Add PHPDoc comments to HandleAlmaUserData methods refs #10372

--- src/Service/HandleAlmaUserData.php
+++ src/Service/HandleAlmaUserData.php
@@ -8,11 +8,21 @@ class HandleAlmaUserData
     private $api_url;
     private $api_key;
 
+  /**
+   * HandleAlmaUserData constructor. Sets the API_URL and API_KEY variables that are set in .env
+   */
     function __construct() {
         $this->api_url = getenv('API_URL');
         $this->api_key = getenv('API_KEY');
     }
 
+  /**
+   * Wrapper for requests to Almas API
+   * @param $uaid - The users uaid (coming from shibboleth server variable property 'Shib-uaId'
+   * @param $queryParams
+   * @param $url
+   * @return SimpleXMLElement
+   */
     public function executeApiRequest($uaid, $queryParams, $url) 
     {
         $ch = curl_init();
@@ -27,7 +37,13 @@ class HandleAlmaUserData
         $sXML = new SimpleXMLElement($response);
         return $sXML;
     }
-    
+
+  /**
+   * A wrapper around the executeApiRequest method, to be called by get methods retrieving user data.
+   * @param $uaid
+   * @param $api_path
+   * @return SimpleXMLElement
+   */
     public function getUserData($uaid, $api_path) {
         $url = $this->api_url . $api_path;
         $templateParamNames = array('{user_id}');
@@ -38,6 +54,11 @@ class HandleAlmaUserData
         return $this->executeApiRequest($uaid, $queryParams, $url);
     }
 
+  /**
+   * Retrieve user fines from alma and return an associative array with each fines id, label, balance, and title
+   * @param $uaid
+   * @return array
+   */
     public function getUserFines($uaid) 
     {
         $fees = $this->getUserData($uaid, '/almaws/v1/users/{user_id}/fees');
@@ -54,16 +75,25 @@ class HandleAlmaUserData
         }
         return $list_fees;
 
-        
-
     }
 
+  /**
+   * Retreive users full name from Alma
+   * @param $uaid
+   * @return SimpleXMLElement
+   */
     public function getUsersFullName($uaid)
     {
         $user = $this->getUserData($uaid, '/almaws/v1/users/{user_id}');
         return $user->full_name;
     }
 
+  /**
+   * Check the uaid coming from Shibboleth with the primary_id from Alma (they should  be the same). If the user with a netid exists there should be
+   * total_record_count equal to '1'. If there is no matching primary_id to uaid in alma, return false.
+   * @param $uaid
+   * @return bool
+   */
     public function almaUserExists($uaid)
     {
         $queryParams = '?' . urlencode('limit') . '=' . urlencode('1') . '&' . urlencode('offset') . '=' . urlencode('0') . '&' . urlencode('q') . '=' . urlencode('primary_id~' . $uaid) . '&' . urlencode('order_by') . '=' . urlencode('last_name, first_name, primary_id');
