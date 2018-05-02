22e1e1516afd19b29a3a3f9a279d2a651f8dfd80
Andy Osborne <cao89@email.arizona.edu>
2018-05-02 11:27:23 -0700

    Add tests for AlmaApi and AlmaUserData services refs #10372

--- tests/Service/AlmaApiTest.php
+++ tests/Service/AlmaApiTest.php
@@ -13,47 +13,44 @@ use PHPUnit\Framework\TestCase;
 use Symfony\Component\Dotenv\Dotenv;
 
 class AlmaApiTest extends TestCase
-{
-  private $api;
-  private $uaid;
+    {
+    private $api;
+    private $uaid;
 
-  public function setUp()
-  {
-      $dotenv = new Dotenv();
-      $dotenv->load(__DIR__.'/../../.env');
-      $this->api = new AlmaApi();
-      $this->uaid = getenv('TEST_UAID');
+    public function setUp()
+    {
+        $dotenv = new Dotenv();
+        $dotenv->load(__DIR__.'/../../.env');
+        $this->api = new AlmaApi();
+        $this->uaid = getenv('TEST_UAID');
 
-      parent::setUp();
-  }
+        parent::setUp();
+    }
 
-  /**
-   * Test that a 200 response code is provided in the response object returned by getUserFines.
-   */
-  public function testGetUserFines()
-  {
-      $userfines = $this->api->getUserFines($this->uaid);
-      $this->assertEquals(200, $userfines->getStatusCode());
-  }
+    /**
+    * Test that a 200 response code is provided in the response object returned by getUserFines.
+    */
+    public function testGetUserFines()
+    {
+        $userfines = $this->api->getUserFines($this->uaid);
+        $this->assertEquals(200, $userfines->getStatusCode());
+    }
 
-//  /**
-//   * Test that a 200 response code is provided in the response object returned by getUsersFullName.
-//   */
-//  public function testGetUsersFullName()
-//  {
-//    $user_full_name = $this->api->getUsersFullName($this->uaid);
-//
-//    $this->assertEquals(200, $user_full_name->getStatusCode());
-//  }
-//
-//  /**
-//   * Test that a 200 response code is provided in the response object returned by getAlmaUser.
-//   */
-//  public function testGetAlmaUser()
-//  {
-//    $userfines = $this->api->getAlmaUser($this->uaid);
-//
-//    $this->assertEquals(200, $userfines->getStatusCode());
-//  }
+    /**
+    * Test that a 200 response code is provided in the response object returned by getUsersFullName.
+    */
+    public function testGetUserById()
+    {
+        $user = $this->api->getUserById($this->uaid);
 
+        $this->assertEquals(200, $user->getStatusCode());
+    }
+
+    public function testFindUserById() {
+        $user = $this->api->findUserById($this->uaid);
+
+        $this->assertEquals(200, $user->getStatusCode());
+    }
 }
+
+
