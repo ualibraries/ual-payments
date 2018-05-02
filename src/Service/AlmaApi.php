f9d3f1fd4c1a4e0a731799a90386e850a9e8ad28
Andy Osborne <cao89@email.arizona.edu>
2018-05-02 11:24:47 -0700

    Add AlmaApi and AlmaUserData service classes refs #10372

--- src/Service/AlmaApi.php
+++ src/Service/AlmaApi.php
@@ -9,75 +9,38 @@ use GuzzleHttp\Psr7;
 
 class AlmaApi
 {
-    private $api_url;
-    private $api_key;
+    private $apiUrl;
+    private $apiKey;
 
   /**
-   * HandleAlmaUserData constructor. Sets the API_URL and API_KEY variables that are set in .env
+   * HandleAlmaUserData constructor. Sets the apiUrl and apiKey variables that are set in .env
    */
     function __construct() {
-        $this->api_url = getenv('API_URL');
-        $this->api_key = getenv('API_KEY');
+        $this->apiUrl = getenv('API_URL');
+        $this->apiKey = getenv('API_KEY');
     }
 
-  /**
-   * Wrapper for requests to Almas API
-   * @param $uaid - The users uaid (coming from shibboleth server variable property 'Shib-uaId'
-   * @param $queryParams
-   * @param $url
-   * @return SimpleXMLElement
-   */
-    public function executeApiRequest($uaid, $queryParams, $url) 
+    /**
+     * Wrapper for requests to Almas API
+     * @param $urlPath
+     * @param $method
+     * @param $queryParams
+     * @param $curlOps
+     * @param $templateParamNames
+     * @param $templateParamValues
+     * @return mixed|null|\Psr\Http\Message\ResponseInterface
+     */
+    public function executeApiRequest($urlPath, $method, $queryParams, $curlOps, $templateParamNames, $templateParamValues)
     {
-        $ch = curl_init();
-        $queryParams .= '&' . urlencode('apikey') . '=' . $this->api_key;
-        curl_setopt($ch, CURLOPT_URL, $url . $queryParams);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-        curl_setopt($ch, CURLOPT_HEADER, FALSE);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
-        $response = curl_exec($ch);
-        curl_close($ch);
-          
-        $sXML = new SimpleXMLElement($response);
-        return $sXML;
-    }
+        $client = new Client(['base_uri' => $this->apiUrl]);
 
-  /**
-   * A wrapper around the executeApiRequest method, to be called by get methods retrieving user data.
-   * @param $uaid
-   * @param $api_path
-   * @return SimpleXMLElement
-   */
-    public function getUserData($uaid, $api_path) {
-        $url = $this->api_url . $api_path;
-        $templateParamNames = array('{user_id}');
-        $templateParamValues = array(urlencode($uaid));
-        $url = str_replace($templateParamNames, $templateParamValues, $url);
-        $queryParams = '?' . urlencode('user_id_type') . '=' . urlencode('all_unique') . '&' . urlencode('view') . '=' . urlencode('full') . '&' . urlencode('expand') . '=' . urlencode('none');
-
-        return $this->executeApiRequest($uaid, $queryParams, $url);
-    }
-
-    public function getUserFines($uaid) 
-    {
-        $client = new Client(['base_uri' => $this->api_url]);
-
-        $url = '/almaws/v1/users/{user_id}/fees';
-        $templateParamNames = array('{user_id}');
-        $templateParamValues = array(urlencode($uaid));
-        $url = str_replace($templateParamNames, $templateParamValues, $url);
+        $url = $urlPath;
+        $url = str_replace($templateParamNames, $templateParamValues, $urlPath);
 
         try {
-            $response = $client->request('GET', $url, [
-                'query' => [
-                    'user_id_type' => 'all_unique',
-                    'status' => 'ACTIVE',
-                    'apikey' => $this->api_key
-                ],
-                'curl' => [
-                    CURLOPT_HEADER => FALSE,
-                    CURLOPT_RETURNTRANSFER => TRUE
-                ]
+            $response = $client->request($method, $url, [
+                'query' => $queryParams,
+                'curl' => $curlOps
             ]);
         } catch (GuzzleException $e) {
             echo Psr7\str($e->getRequest());
@@ -87,32 +50,79 @@ class AlmaApi
             return null;
         }
         return $response;
+
     }
 
-  /**
-   * Retreive users full name from Alma
-   * @param $uaid
-   * @return SimpleXMLElement
-   */
-    public function getUsersFullName($uaid)
+    /**
+     * Get the users list of fines from Alma
+     * @param $uaid
+     * @return mixed|null|\Psr\Http\Message\ResponseInterface
+     */
+    public function getUserFines($uaid) 
     {
-        $user = $this->getUserData($uaid, '/almaws/v1/users/{user_id}');
-        return $user->full_name;
+        $method = 'GET';
+        $urlPath = '/almaws/v1/users/{user_id}/fees';
+        $templateParamNames = array('{user_id}');
+        $templateParamValues = array(urlencode($uaid));
+        $queryParams = [
+            'user_id_type' => 'all_unique',
+            'status' => 'ACTIVE',
+            'apikey' => $this->apiKey
+        ];
+        $curlOps = [
+            CURLOPT_HEADER => FALSE,
+            CURLOPT_RETURNTRANSFER => TRUE
+        ];
+
+        return $this->executeApiRequest($urlPath, $method, $queryParams, $curlOps, $templateParamNames, $templateParamValues);
     }
 
-  /**
-   * Check the uaid coming from Shibboleth with the primary_id from Alma (they should  be the same). If the user with a netid exists there should be
-   * total_record_count equal to '1'. If there is no matching primary_id to uaid in alma, return false.
-   * @param $uaid
-   * @return bool
-   */
-    public function getAlmaUser($uaid)
+    /**
+     * Get the user from alma by the user id. Returns 400 status code if user does not exist.s
+     * @param $uaid
+     * @return mixed|null|\Psr\Http\Message\ResponseInterface
+     */
+    public function getUserById($uaid)
     {
-        $queryParams = '?' . urlencode('limit') . '=' . urlencode('1') . '&' . urlencode('offset') . '=' . urlencode('0') . '&' . urlencode('q') . '=' . urlencode('primary_id~' . $uaid) . '&' . urlencode('order_by') . '=' . urlencode('last_name, first_name, primary_id');
-        $url = $this->api_url .  '/almaws/v1/users';
-        $users = $this->executeApiRequest($uaid,$queryParams, $url);
+        $method = 'GET';
+        $urlPath = '/almaws/v1/users/{user_id}';
+        $templateParamNames = array('{user_id}');
+        $templateParamValues = array(urlencode($uaid));
+        $queryParams = [
+            'user_id_type' => 'all_unique',
+            'view' => 'full',
+            'expand' => 'none',
+            'apikey' => $this->apiKey
+        ];
+        $curlOps = [
+            CURLOPT_HEADER => FALSE,
+            CURLOPT_RETURNTRANSFER => TRUE
+        ];
+        return $this->executeApiRequest($urlPath, $method, $queryParams, $curlOps, $templateParamNames, $templateParamValues);
+    }
 
-        return $users->attributes()->total_record_count == '1';
-
+    /**
+     * Use the Alma api to search for the user by primary_id. This is how we will check that a the provided uaid is found
+     * in Alma as a primary_id.
+     * @param $uaid
+     * @return mixed|null|\Psr\Http\Message\ResponseInterface
+     */
+    public function findUserById($uaid) {
+        $method = 'GET';
+        $urlPath = '/almaws/v1/users';
+        $templateParamNames = array();
+        $templateParamValues = array();
+        $queryParams = [
+            'limit' => '10',
+            'offset' => '0',
+            'q' => 'primary_id~' . $uaid,
+            'order_by' => 'last_name, first_name, primary_id',
+            'apikey' => $this->apiKey
+        ];
+        $curlOps = [
+            CURLOPT_HEADER => FALSE,
+            CURLOPT_RETURNTRANSFER => TRUE
+        ];
+        return $this->executeApiRequest($urlPath, $method, $queryParams, $curlOps, $templateParamNames, $templateParamValues);
     }
 }
