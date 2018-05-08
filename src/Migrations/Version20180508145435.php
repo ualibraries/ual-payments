96f703e2ed6c3b231c5fae6ae5ae00c99590c002
Andy Osborne <cao89@email.arizona.edu>
2018-05-08 09:12:49 -0700

    Add migration for Transaction and Fee entity refs #10382

---
+++ src/Migrations/Version20180508145435.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20180508145435 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE fee (id INT AUTO_INCREMENT NOT NULL, transaction_id INT NOT NULL, fee_id VARCHAR(25) NOT NULL, label VARCHAR(255) DEFAULT NULL, balance NUMERIC(7, 2) NOT NULL, INDEX IDX_964964B52FC0CB0F (transaction_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE transaction (id INT AUTO_INCREMENT NOT NULL, date DATETIME NOT NULL, status VARCHAR(255) NOT NULL, invoice_number VARCHAR(25) NOT NULL, user_id VARCHAR(25) NOT NULL, total_balance NUMERIC(7, 2) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE fee ADD CONSTRAINT FK_964964B52FC0CB0F FOREIGN KEY (transaction_id) REFERENCES transaction (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE fee DROP FOREIGN KEY FK_964964B52FC0CB0F');
+        $this->addSql('DROP TABLE fee');
+        $this->addSql('DROP TABLE transaction');
+    }
+}
