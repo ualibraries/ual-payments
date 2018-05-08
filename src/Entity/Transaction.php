e36db04c527c407282269823b6f4e8a5b6f20fa3
Andy Osborne <cao89@email.arizona.edu>
2018-05-07 17:48:06 -0700

    Add Transaction and Fee Entities refs #10382

---
+++ src/Entity/Transaction.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\TransactionRepository")
+ */
+class Transaction
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $status;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Fee", mappedBy="transaction_id", orphanRemoval=true)
+     */
+    private $fees;
+
+    /**
+     * @ORM\Column(type="string", length=25)
+     */
+    private $invoice_number;
+
+    /**
+     * @ORM\Column(type="string", length=25)
+     */
+    private $user_id;
+
+    public function __construct()
+    {
+        $this->fees = new ArrayCollection();
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Fee[]
+     */
+    public function getFees(): Collection
+    {
+        return $this->fees;
+    }
+
+    public function addFee(Fee $fee): self
+    {
+        if (!$this->fees->contains($fee)) {
+            $this->fees[] = $fee;
+            $fee->setTransaction($this);
+        }
+
+        return $this;
+    }
+
+    public function removeFee(Fee $fee): self
+    {
+        if ($this->fees->contains($fee)) {
+            $this->fees->removeElement($fee);
+            // set the owning side to null (unless already changed)
+            if ($fee->getTransaction() === $this) {
+                $fee->setTransaction(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getInvoiceNumber(): ?string
+    {
+        return $this->invoice_number;
+    }
+
+    public function setInvoiceNumber(string $invoice_number): self
+    {
+        $this->invoice_number = $invoice_number;
+
+        return $this;
+    }
+
+    public function getUserId(): ?string
+    {
+        return $this->user_id;
+    }
+
+    public function setUserId(string $user_id): self
+    {
+        $this->user_id = $user_id;
+
+        return $this;
+    }
+}
