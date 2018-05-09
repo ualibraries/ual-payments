52c8651ffbfad195604e913101100a9539fce778
Christopher Green <cg433n@email.arizona.edu>
2018-05-09 09:11:00 -0700

    Adds front-end scaffolding Refs #10382

---
+++ assets/components/Header.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import HelpControl from './HelpControl'
+import branding from '../images/branding.svg'
+
+class Header extends React.Component {
+  state = {
+    helpControlActive: false
+  }
+
+  /**
+   * Handle a click event on the help control.
+   */
+  helpControlHandler = () => {
+    this.setState({
+      helpControlActive: !this.state.helpControlActive,
+      settingsControlActive: false
+    })
+  }
+
+  /**
+   * Handler closes help and settings control.
+   */
+  globalExitHandler = event => {
+    let resetState = () => {
+      this.setState({
+        helpControlActive: false
+      })
+    }
+
+    !document.getElementById('header__uicontrols').contains(event.target)
+      ? resetState()
+      : null
+  }
+
+  /**
+   * Bind the global exit handler after the component mounts.
+   */
+  componentDidMount() {
+    document.body.addEventListener('click', this.globalExitHandler)
+  }
+
+  /**
+   * Render component.
+   */
+  render() {
+    return (
+      <div className="Header">
+        <div className="header__inner">
+          <a
+            href="https://new.library.arizona.edu"
+            className="logo"
+            dangerouslySetInnerHTML={{ __html: branding }}
+          />
+          <div className="header__uicontrols-outer">
+            <div className="header__uicontrols" id="header__uicontrols">
+              <HelpControl
+                handler={this.helpControlHandler}
+                open={this.state.helpControlActive}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Header
