bfa12a82f653caa1a9a40908e7440a271192c838
Will Simpson <wsimpson@email.arizona.edu>
2018-05-11 12:39:51 -0700

    Add db backup and migrations to deployer.  Refs #10394

--- deploy.php
+++ deploy.php
@@ -43,6 +43,14 @@ task('build', function () {
     run('cd {{release_path}} && build');
 });
 
+// Backup remote database
+task('backup-remote-db', function () {
+    cd('{{release_path}}');
+    run('source .env && mysqldump -u $DB_USER -p$DB_PASS $DB_DATABASE | gzip > ./backups/$DB_DATABASE-`date +%s`.sql.gz');
+    // Remove database backup files older than 30 days
+    run('find ./backups -name *sql.gz -mtime 30 -type f -delete');
+});
+
 desc('Deploy project');
 task('deploy', [
     'deploy:info',
@@ -55,6 +63,8 @@ task('deploy', [
     'deploy:vendors',
     'deploy:cache:clear',
     'deploy:cache:warmup',
+    'backup-remote-db',
+    'database:migrate',
     'deploy:symlink',
     'deploy:unlock',
     'cleanup',
