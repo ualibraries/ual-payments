45b1749c11058f24026fb1238ab8360907a7bb72
Ruiqi Yu <richyu@email.arizona.edu>
2018-05-23 16:02:20 -0700

    authentication method in AlmaApi with PHPUnit tests Refs #10461

--- tests/Service/AlmaApiTest.php
+++ tests/Service/AlmaApiTest.php
@@ -11,29 +11,28 @@ namespace App\Tests\Service;
 use App\Service\AlmaApi;
 use App\Service\AlmaUserData;
 use PHPUnit\Framework\TestCase;
+use SimpleXMLElement;
 use Symfony\Component\Dotenv\Dotenv;
-use \SimpleXMLElement;
 
 class AlmaApiTest extends TestCase
 {
     private $api;
-    private $testFee;
     private $userId;
-    private $userdata;
+    private $userData;
 
     public function setUp()
     {
         $dotenv = new Dotenv();
-        $dotenv->load(__DIR__.'/../../.env');
+        $dotenv->load(__DIR__ . '/../../.env');
         $this->api = new AlmaApi();
         $this->userId = getenv('TEST_ID');
-        $this->userdata = new AlmaUserData($this->userId);
+        $this->userData = new AlmaUserData($this->userId);
         parent::setUp();
     }
 
     /**
-    * Test that a 200 response code is provided in the response object returned by getUserFees.
-    */
+     * Test that a 200 response code is provided in the response object returned by getUserFees.
+     */
     public function testGetUserFees()
     {
         $userFees = $this->api->getUserFees($this->userId);
@@ -41,8 +40,8 @@ class AlmaApiTest extends TestCase
     }
 
     /**
-    * Test that a 200 response code is provided in the response object returned by getUsersFullName.
-    */
+     * Test that a 200 response code is provided in the response object returned by getUsersFullName.
+     */
     public function testGetUserById()
     {
         $user = $this->api->getUserById($this->userId);
@@ -59,29 +58,56 @@ class AlmaApiTest extends TestCase
 
     public function testPayUserFee()
     {
+        $feeNotRemoved = false;
         try {
             $testFeeBody = file_get_contents(__DIR__ . '/TestJSONData/fee1.json');
             $testFee = $this->createFeeForTesting($testFeeBody);
             $this->api->payUserFee($this->userId, $testFee->id, (float)$testFee->balance);
             $response = $this->api->getUserFees($this->userId);
-            $userFees = $this->userdata->listFees($response);
+            $userFees = $this->userData->listFees($response);
 
-            $feeNotRemoved = false;
             foreach ($userFees as $fee) {
-                if($fee['id'] == $testFee->id) {
+                if ($fee['id'] == $testFee->id) {
                     $feeNotRemoved = true;
                 }
             }
         } catch (\Exception $e) {
-            $this->fail("Unable to pay user test fee: " . $e->getMessage());
+            $this->fail('Unable to pay user test fee: ' . $e->getMessage());
         }
 
         $this->assertFalse($feeNotRemoved);
     }
 
-    private function createFeeForTesting($testFeeBody) {
+    public function testAuthenticateUser()
+    {
+        //Test correct password
+        try {
+            $response = $this->api->authenticateUser($this->userId, getenv('TEST_PASS'));
+            $this->assertEquals(204, $response->getStatusCode());
+        } catch (\Exception $e) {
+            $this->fail($e->getMessage());
+        }
+
+        //Test wrong password
+        try {
+            $this->api->authenticateUser($this->userId, uniqid());
+            $this->fail('Test wrong password failed');
+        } catch (\GuzzleHttp\Exception\TransferException $te) {
+            $this->assertEquals(400, $te->getCode());
+        }
+
+        //Test random userId
+        try {
+            $this->api->authenticateUser(uniqid(), uniqid());
+            $this->fail('Test random userId failed');
+        } catch (\GuzzleHttp\Exception\TransferException $te) {
+            $this->assertEquals(400, $te->getCode());
+        }
+    }
+
+    private function createFeeForTesting($testFeeBody)
+    {
         $response = $this->api->createUserFee($this->userId, json_decode($testFeeBody));
         return new SimpleXMLElement($response->getBody());
     }
-
 }
