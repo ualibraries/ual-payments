4ac7c15928b234c11d44a169a1c6af5a019e42e0
Ruiqi Yu <richyu@email.arizona.edu>
2018-05-25 14:23:38 -0700

    Fixed AlmaUserProvider Refs #10460

--- src/Security/User/AlmaUserProvider.php
+++ src/Security/User/AlmaUserProvider.php
@@ -1,31 +1,25 @@
 <?php
+
 namespace App\Security\User;
 
-use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\Security\Core\User\User;
 use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
 
 class AlmaUserProvider implements UserProviderInterface
 {
     public function loadUserByUsername($username)
     {
-        return new User(
-            $username,
-            null,
-            // the roles for the user - you may choose to determine
-            // these dynamically somehow based on the user
-            array('ROLE_USER')
-        );
+        return new User($username, null, array('ROLE_USER'));
     }
 
     public function refreshUser(UserInterface $user)
     {
-        // this is used for storing authentication in the session
-        // but in this example, the token is sent in each request,
-        // so authentication can be stateless. Throwing this exception
-        // is proper to make things stateless
-        throw new UnsupportedUserException();
+        if (!$user instanceof User) {
+            throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', get_class($user)));
+        }
+        return $this->loadUserByUsername($user->getUsername());
     }
 
     public function supportsClass($class)
