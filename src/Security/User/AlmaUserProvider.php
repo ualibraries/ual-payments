d3ec3b36f965979a08d4566da7e25018717cf387
Ruiqi Yu <richyu@email.arizona.edu>
2018-07-12 10:38:15 -0700

    Updated AlmaUserProvider to use AlmaUser Refs #10509

--- src/Security/User/AlmaUserProvider.php
+++ src/Security/User/AlmaUserProvider.php
@@ -3,7 +3,6 @@
 namespace App\Security\User;
 
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
-use Symfony\Component\Security\Core\User\User;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 
@@ -11,19 +10,19 @@ class AlmaUserProvider implements UserProviderInterface
 {
     public function loadUserByUsername($username)
     {
-        return new User($username, null, array('ROLE_USER'));
+        return new AlmaUser($username, array('ROLE_USER'));
     }
 
     public function refreshUser(UserInterface $user)
     {
-        if (!$user instanceof User) {
+        if (!$user instanceof AlmaUser) {
             throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', get_class($user)));
         }
-        return $this->loadUserByUsername($user->getUsername());
+        return $user;
     }
 
     public function supportsClass($class)
     {
-        return User::class === $class;
+        return AlmaUser::class === $class;
     }
 }
