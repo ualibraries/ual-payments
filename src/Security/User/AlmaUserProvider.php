33b2c823520140f683670627cb40b43c8a51ff62
Will Simpson <wsimpson@email.arizona.edu>
2018-07-26 11:29:26 -0700

    Add logging for AlmaUserProvider::loadeUserByUsername.  Refs #10509

--- src/Security/User/AlmaUserProvider.php
+++ src/Security/User/AlmaUserProvider.php
@@ -8,16 +8,18 @@ use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Psr\Log\LoggerInterface;
 
 class AlmaUserProvider implements UserProviderInterface
 {
     private $api;
     private $userData;
 
-    public function __construct(AlmaApi $api, AlmaUserData $userData)
+    public function __construct(AlmaApi $api, AlmaUserData $userData, LoggerInterface $logger)
     {
         $this->api = $api;
         $this->userData = $userData;
+        $this->logger = $logger;
     }
 
     public function loadUserByUsername($username)
@@ -30,7 +32,7 @@ class AlmaUserProvider implements UserProviderInterface
                 return new AlmaUser($username, array('ROLE_USER'), $firstName, $lastName);
             }
         } catch (\GuzzleHttp\Exception\GuzzleException $e) {
-            echo $e->getCode() . $e->getMessage();
+            $this->logger->error($e->getCode() . $e->getMessage());
         }
 
         throw new UsernameNotFoundException(sprintf('Username "%s" does not exist.', $username));
