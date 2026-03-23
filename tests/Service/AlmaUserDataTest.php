6e825fe6b8159bd54c557d613750f1d6a655598f
Rich Yu <zzcjbyrq@gmail.com>
2026-03-22 22:48:16 -0700

    Add a test for responseHasFees(). Refs #16867

--- tests/Service/AlmaUserDataTest.php
+++ tests/Service/AlmaUserDataTest.php
@@ -4,23 +4,18 @@ namespace App\Tests\Service;
 
 use App\Service\AlmaUserData;
 use PHPUnit\Framework\TestCase;
-use App\Service\AlmaApi;
 use GuzzleHttp\Client;
 use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Psr7\Response;
 use GuzzleHttp\Psr7;
 use GuzzleHttp\Exception\GuzzleException;
-use GuzzleHttp\Psr7\Request;
-use GuzzleHttp\Exception\RequestException;
 use Symfony\Component\Dotenv\Dotenv;
 
-class AlmaUserDataTest extends TestCase
-{
+class AlmaUserDataTest extends TestCase {
     private $userdata;
 
-    public function setUp(): void
-    {
+    public function setUp(): void {
         (new Dotenv())->loadEnv(__DIR__ . '/../../.env');
         $userId = $_ENV['TEST_ID'];
         $this->userdata = new AlmaUserData($userId);
@@ -31,8 +26,7 @@ class AlmaUserDataTest extends TestCase
     /**
      * Test getting fees when the user has multiple fees.
      */
-    public function testListFeesWithMultipleFees()
-    {
+    public function testListFeesWithMultipleFees() {
         $body = file_get_contents(__DIR__ . '/TestXMLData/fees_test_data.xml');
         $mock = new MockHandler([
             new Response(200, [], $body)
@@ -48,7 +42,6 @@ class AlmaUserDataTest extends TestCase
                 echo Psr7\str($e->getResponse());
             }
         }
-
 
         $givenListOfFees = $this->userdata->listFees($response);
 
@@ -75,10 +68,35 @@ class AlmaUserDataTest extends TestCase
     }
 
     /**
+     * Test responseHasFees when the user has transfer fees.
+     */
+    public function testResponseHasFees() {
+        $body = file_get_contents(__DIR__ . '/TestXMLData/transfer_fee_data.xml');
+        $mock = new MockHandler([
+            new Response(200, [], $body)
+        ]);
+        $handler = HandlerStack::create($mock);
+        $client = new Client(['handler' => $handler]);
+
+        try {
+            $response = $client->request('/');
+        } catch (GuzzleException $e) {
+            echo Psr7\str($e->getRequest());
+            if ($e->hasResponse()) {
+                echo Psr7\str($e->getResponse());
+            }
+        }
+
+        $given = $this->userdata->responseHasFees($response);
+        $expected = true;
+
+        $this->assertEquals($expected, $given);
+    }
+
+    /**
      * Test getting the users full name from Alma
      */
-    public function testGetFullNameAsString()
-    {
+    public function testGetFullNameAsString() {
         $body = file_get_contents(__DIR__ . '/TestXMLData/user_test_data.xml');
         $mock = new MockHandler([
             new Response(200, [], $body)
@@ -103,8 +121,7 @@ class AlmaUserDataTest extends TestCase
     /**
      * Test that a user not found in Alma returns false
      */
-    public function testCheckInvalidUser()
-    {
+    public function testCheckInvalidUser() {
         $body = file_get_contents(__DIR__ . '/TestXMLData/no_user_found_data.xml');
 
         $mock = new MockHandler([
@@ -131,8 +148,7 @@ class AlmaUserDataTest extends TestCase
     /**
      * Test that the Shib-uaId matches a valid primary_id in Alma
      */
-    public function testCheckValidUser()
-    {
+    public function testCheckValidUser() {
         $body = file_get_contents(__DIR__ . '/TestXMLData/find_user_data.xml');
 
         $mock = new MockHandler([
